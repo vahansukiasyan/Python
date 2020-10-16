e6c19b395429984122857bacf0036de4dc710509
vahansukiasyan <vahansukiasyan2@gmail.com>
2020-10-16 12:41:24 +0400

    homeworks

---
+++ homework 2.py
@@ -0,0 +1,21 @@
+# exercise 1
+
+#num = input()
+#a = []
+#my_dict = {}
+#while num != "End":
+    #a.append(num)
+    #num = input()
+#for elem in a:
+    #if elem in my_dict:
+        #continue
+    #else:
+        #my_dict[int(elem)] = a.count(elem)
+#print(my_dict)
+
+#exercise 4
+
+for i in range(5):
+    for j in range(i):
+        print(i,end=' ')
+    print(' ')
---
+++ homework 27.py
@@ -0,0 +1,26 @@
+# exercise 1
+
+#s = "Hello!I am Ruben. how are you?" 
+#s = s.replace(".", " ")
+#s = s.replace("?", " ")
+#s = s.replace("!", " ")
+#print(s)
+
+# exercise 2
+
+#n = [int(x) for x in input()]
+#print(round(sum(n)/len(n),1))
+    
+# exercise 3
+
+#text = input().split()
+#count = 0
+#for word in text:
+    #if word[0] == word[0].upper():
+        #count += 1
+#print(count)
+
+# exercise 4
+
+#text = input()
+#if text[0].isdigit() == False
