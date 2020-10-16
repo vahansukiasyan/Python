429fdc1472d0d426e92d507cd4a04cf70fdd38d0
vahansukiasyan <vahansukiasyan2@gmail.com>
2020-10-16 13:02:06 +0400

    Everything working

--- homework 2.py
+++ homework 2.py
@@ -1,21 +1,49 @@
-# exercise 1
+#Ruben
 
-#num = input()
-#a = []
-#my_dict = {}
-#while num != "End":
-    #a.append(num)
-    #num = input()
-#for elem in a:
-    #if elem in my_dict:
-        #continue
-    #else:
-        #my_dict[int(elem)] = a.count(elem)
-#print(my_dict)
+#exercise 1
 
-#exercise 4
+n = input()
+nums = []
+d = {}
+while n != "End":
+    nums.append(n)
+    n = input()
+for m in nums:
+    if m in d:
+        continue
+    else:
+        d[int(m)] = nums.count(m)
+print(d)
 
-for i in range(5):
-    for j in range(i):
-        print(i,end=' ')
-    print(' ')
+#exercise 2
+
+text = input()
+d = {}
+x = 0
+for i in text:
+    if i in d:
+        d[i] += 1
+    else:
+        d[i] = 1
+for val in d.values():
+    if val % 2 == 1:
+        x+=1
+if len(text) % 2 == 0:
+    print(False if x>=1 else True)
+if len(text) % 2 == 1:
+    print(False if x>=2 else True)
+#Narek 
+
+exercise 1
+for i in range(100):
+    for j in range(100):
+        if i ** j == j**i and i!=j!=i**j:
+            print(i,j,i**j, )
+    
+
+#exercise 2
+
+for row in range(1,5):
+    for i in range(1,row+1):
+        print(row, end=" ")  
+    print(" ")
