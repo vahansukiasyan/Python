a46d7b08e92514e09f8c71da1c43cb34d6463669
vahansukiasyan <vahansukiasyan2@gmail.com>
2020-11-09 21:02:20 +0400

    Homework 9

--- homework 8.py
+++ homework 8.py
@@ -3,36 +3,63 @@
 #exercise 1
 
 #def big(a):
-    #return a[-1]*a[-2]*a[-3]
-#print(big(sorted([9,5,8,5,20,1,2,-3,-2,-1,0])))
+    #b = [i for i in a if i<0]
+    #b.sort(reverse = True)
+    #if len(b)<2:
+        #return a[-1]*a[-2]*a[-3]
+    #else:
+        #if a[-1]*a[-2]*a[-3] >= a[-1]*(b[-1]*b[-2]):
+            #return a[-1]*a[-2]*a[-3]
+        #else:
+            #return a[-1]*(b[-1]*b[-2])
+#print(big(sorted([9,5,8,5,20,1,2,-3,-2,0])))
 
 #exercise 2
 
-#def author(auth,auth1):
-#    print(auth,auth1)
-#author("Vahan",auth1=["Vahan",2006])
+#def author(name,**rest):
+    #d = {}
+    #for key,value in rest.items():
+        #if value[0] == name:
+            #d[key] = value[1]
+    #srt_d = sorted((value,key) for (key,value) in d.items())
+    #for i in srt_d:
+        #print(i[1],i[0])
+#author("Karen",auth_1=["Karen",2006],auth_2 = ["Narek",1957],auth_3=["Andrew",1374],auth_4 = ["Karen",1769])
 
 #Ruben
 
 #exercise 1
 
 #def answer_queries(k, *query_counts):
-    #return (sum(query_counts) // k) + 1
-#print(answer_queries(1,5,100))
+    #m  = 0
+    #for i in range(1,len([*query_counts])+1):
+        #if [*query_counts][i-1] + m - k  >=0:
+            #if i == len([*query_counts]):
+                #return int((([*query_counts][-1]+m)/k)+i) 
+            #if [*query_counts][i-1]+ m - k > 0:
+                #m = [*query_counts][i-1]+ m - k
+        #else:
+            #return i
+#print(answer_queries(1,100))
 
 
 #exercise 2
     
 #def non_decreasing_sequence(*numbers):
     #my_list = [*numbers]
-    #for i in range(len(my_list)):
-        #for j in range(len(my_list)):
-            #my_list[i] =  -my_list[i]
-            #my_list[j] =  -my_list[j]
-            #if my_list == sorted(my_list):
-                #return 'Yes',my_list
-            #else:
+    #if my_list == sorted(my_list,reverse = True):
+        #for i in range(len(my_list)):
+            #my_list[i] = -my_list[i]
+        #return "Yes",my_list
+    #else:
+        #for i in range(len(my_list)):
+            #for j in range(len(my_list)):
                 #my_list[i] =  -my_list[i]
                 #my_list[j] =  -my_list[j]
-    #return 'No'
-#print(non_decreasing_sequence(0,4,3))    
+                #if my_list == sorted(my_list):
+                    #return 'Yes',my_list
+                #else:
+                    #my_list[i] =  -my_list[i]
+                    #my_list[j] =  -my_list[j]
+        #return 'No'
+#print(non_decreasing_sequence(7,-4,3,2))    
---
+++ homework 9.py
@@ -0,0 +1,45 @@
+#Narek
+
+#exercise 1
+
+#def fake_recursion(my_list):
+    #if len(my_list) == 1 and my_list[0] == 1:
+        #print(a.index(1))
+    #else:
+        #if sum(my_list[:len(my_list)//2]) % 2 == 1:
+            #fake_recursion(my_list[:len(my_list)//2])
+        #elif sum(my_list[:len(my_list)//2]) % 2 == 0:
+            #fake_recursion(my_list[len(my_list)//2:])
+#a = [2,2,2,1,2,2,2,2]
+#fake_recursion(a)
+
+#exerxise 2
+
+#def recursive_string(string):
+    #if '(' in string:
+        #for i in range(len(string)):
+            #if string[i] == '(':
+                #start = i
+                
+            #elif string[i] == ')':
+                #end = i
+        #print(string[:start])
+        #recursive_string(string[:start] + string[start+1:end-1][::-1] + string[end:])
+    #return string
+    
+#print(recursive_string("foo(bar(baz))blim"))
+
+#def buildPalindrome(string):
+    #st = ''
+    #a = string 
+    #for i in range(len(string)):
+        #if a != a[::-1]:
+            #if i>0:
+                #a = string
+            #st = st + string[i]
+            #a+=st[::-1]
+    #return a           
+#a = "abcd"
+#print(buildPalindrome(a))
+            
+            
